14eb4007f377702db21d37cc904a7664f8ad5fde
hunghly <hunghly@gmail.com>
2020-09-12 21:41:13 -0500

    converted input box into a search box component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
 
 class App extends Component {
   constructor() {
@@ -24,30 +25,24 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
 
-    const {monsters, searchField} = this.state;
-
-    const filteredMonster = monsters.filter((monster)=>{
-      if(monster.name.toLowerCase().includes(searchField.toLowerCase())) {
+    const filteredMonster = monsters.filter((monster) => {
+      if (monster.name.toLowerCase().includes(searchField.toLowerCase())) {
         return monster;
       }
-    })
+    });
 
-    console.log('====================================');
+    console.log("====================================");
     console.log("filtered monsters: " + filteredMonster);
-    console.log('====================================');
+    console.log("====================================");
 
     return (
       <div className="App">
         {/* Set State is an Asynchronous event so we have to do any outputs after the rerender or you can pass a callback as the second parameter of the setState */}
-        <input
-          type="search"
-          placeholder="search monsters..."
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value }, () =>
-              console.log(this.state)
-            );
-          }}
+        <SearchBox
+          placeholder="Search Monsters..."
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonster} />
       </div>
