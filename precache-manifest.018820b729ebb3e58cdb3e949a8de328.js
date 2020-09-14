fc80fb5f6de6c6759a2f8f50550012ea1bd736af
hunghly <hunghly@gmail.com>
2020-09-14 14:14:48 -0500

    Updates

---
+++ precache-manifest.018820b729ebb3e58cdb3e949a8de328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68541e463fc3bbd1a00ddb75b3a9338",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "b49454e8fe2c01ecb8cb",
+    "url": "/MonstersRolodex/static/css/main.ac1a2286.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonstersRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49454e8fe2c01ecb8cb",
+    "url": "/MonstersRolodex/static/js/main.870692db.chunk.js"
+  },
+  {
+    "revision": "425f1fd85b5840b582ed",
+    "url": "/MonstersRolodex/static/js/runtime-main.2b75c539.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MonstersRolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
